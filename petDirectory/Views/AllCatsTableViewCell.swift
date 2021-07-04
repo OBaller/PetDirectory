b4902065ecaaf2d51c1fd048526a4cb4d70290b4
Naseem Oyebola <naseemoyebola@gmail.com>
2021-07-04 20:36:45 +0100

    feat: add SweetLike

--- petDirectory/Views/AllCatsTableViewCell.swift
+++ petDirectory/Views/AllCatsTableViewCell.swift
@@ -6,14 +6,22 @@
 //
 import Kingfisher
 import UIKit
+import SweetLike
 
 class AllCatsTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var catImage: UIImageView!
     @IBOutlet weak var catName: UILabel!
     
     
     @IBAction func didTapFavourite(_ sender: Any) {
+    }
+    
+    private let button: SweetLike = SweetLike(frame: CGRect(x: (UIScreen.main.bounds.width / 2) + 175, y: 25, width: 17.68, height: 15.67))
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.addSubview(button)
     }
     
     func setUp(with cat: CatModel) {
