6ab04469c7cf844e4e727fe444cbd207d8f8439d
Naseem Oyebola <naseemoyebola@gmail.com>
2021-07-05 01:08:32 +0100

    feat: favourite cat

--- petDirectory/Views/AllCatsTableViewCell.swift
+++ petDirectory/Views/AllCatsTableViewCell.swift
@@ -6,22 +6,41 @@
 //
 import Kingfisher
 import UIKit
-import SweetLike
 
+protocol AllCatsTableViewCellDelegate: AnyObject {
+    func didTapFavButton (save name: String, and image: UIImage)
+    func didUntapFavButton (remove name: String, and image: UIImage)
+}
 class AllCatsTableViewCell: UITableViewCell {
     
+    weak var delegate: AllCatsTableViewCellDelegate?
     @IBOutlet weak var catImage: UIImageView!
     @IBOutlet weak var catName: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
     
     
-    @IBAction func didTapFavourite(_ sender: Any) {
+    @IBAction func didTapFavourite(_ sender: UIButton) {
+        let redColor = UIColor(red: 222/255, green: 2/255, blue: 2/255, alpha: 1)
+        let greyColor = UIColor(red: 33/255, green: 34/255, blue: 39/255, alpha: 1)
+        sender.tintColor = sender.tintColor == redColor ? greyColor : redColor
+        let favDogImg = UIImage(systemName: "suit.heart.fill")
+        let dogImg = UIImage(systemName: "heart")
+        if sender.tintColor == redColor {
+            likeButton.setImage(favDogImg, for: .normal)
+            
+            delegate?.didTapFavButton(save: catName.text ?? "", and: catImage.image!)
+            print("Favourite dog selected")
+        } else {
+            likeButton.setImage(dogImg, for: .normal)
+            print("Favourite dog removed")
+        }
+ 
     }
     
-    private let button: SweetLike = SweetLike(frame: CGRect(x: (UIScreen.main.bounds.width / 2) + 175, y: 25, width: 17.68, height: 15.67))
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        contentView.addSubview(button)
+        
     }
     
     func setUp(with cat: CatModel) {
